bef162f57127a5da2b7da9e382699d43f7e64bee
MDaud32 <theprofessor543@gmail.com>
2023-10-28 10:51:01 +0500

    adding routes and auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>hello</>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+
+        {/* Private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
